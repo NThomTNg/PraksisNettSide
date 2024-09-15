7a30795c78c320a8b6f0d29a001fb2f7c98e8af3
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-09-15 21:31:15 +0200

    build: Added Flowbite and new dependencies. Feit update.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // enable dark mode via class strategy
+  darkMode: 'class',
+  
+  // add the folders and files from your templates
   content: [
     "./*.html", // Adjust the paths according to your project structure
     "./pages/*.html", // Include other directories as needed
+    "./layouts/**/*.html",
+    "./content/**/*.md",
+    "./content/**/*.html",
+    "./src/**/*.js"
   ],
+
+  // make sure to safelist these classes when using purge
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      // extend base Tailwind CSS utility classes
+    }
   },
-  plugins: [],
+  
+  plugins: [
+    // include Flowbite as a plugin in your Tailwind CSS project
+    require('flowbite/plugin')
+  ]
 };
