d75c24f1ddc1baa541113daf86f6cf099bca04b0
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-10-12 15:35:41 +0200

    refactor: Update image loading styles in status.html and index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // enable dark mode via class strategy
-  darkMode: 'class',
-  
+  darkMode: "class",
+
   // add the folders and files from your templates
   content: [
     "./*.html", // Adjust the paths according to your project structure
@@ -10,33 +10,39 @@ module.exports = {
     "./layouts/**/*.html",
     "./content/**/*.md",
     "./content/**/*.html",
-    "./src/**/*.js"
+    "./src/**/*.js",
   ],
 
   // make sure to safelist these classes when using purge
   safelist: [
-    'w-64',
-    'w-1/2',
-    'rounded-l-lg',
-    'rounded-r-lg',
-    'bg-gray-200',
-    'grid-cols-4',
-    'grid-cols-7',
-    'h-6',
-    'leading-6',
-    'h-9',
-    'leading-9',
-    'shadow-lg'
+    "w-64",
+    "w-1/2",
+    "rounded-l-lg",
+    "rounded-r-lg",
+    "bg-gray-200",
+    "grid-cols-4",
+    "grid-cols-7",
+    "h-6",
+    "leading-6",
+    "h-9",
+    "leading-9",
+    "shadow-lg",
   ],
 
   theme: {
     extend: {
+      colors: {
+        slblue: {
+          DEFAULT: "#13267f",
+          500: "#13267f",
+        },
+      },
       // extend base Tailwind CSS utility classes
-    }
+    },
   },
-  
+
   plugins: [
     // include Flowbite as a plugin in your Tailwind CSS project
-    require('flowbite/plugin')
-  ]
+    require("flowbite/plugin"),
+  ],
 };
